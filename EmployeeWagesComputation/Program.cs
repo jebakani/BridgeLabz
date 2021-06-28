19191af377563cff7f6955d57bf8c1fcf2d6ec69
Jebakani <vjishwarya@gmail.com>
2021-06-28 14:17:32 +0530

    [Jebakani] Adding-UC6-Calculate wages of employee for 100hr or 20 days

--- EmployeeWagesComputation/Program.cs
+++ EmployeeWagesComputation/Program.cs
@@ -9,6 +9,8 @@ namespace EmployeeWagesComputation
         const int IS_PART_TIME = 2;
         const int EMP_RATE_PER_HR = 20;
         const int TOTAL_WORKING_DAY = 20;
+        const int TOTAL_WORKING_HR = 100;
+
 
         /// <summary>
         /// UC1- Finding out whether employee is present or absent
@@ -16,6 +18,7 @@ namespace EmployeeWagesComputation
         /// UC3- Adding Part time employee and Calculate the Wages
         /// UC4- Calculating above UC using switch case
         /// UC5- Calculating Monthly Wages of Employee
+        /// UC6- Calculating The wages till the condition of 100hr or for 20days
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
@@ -25,11 +28,14 @@ namespace EmployeeWagesComputation
             int empHrs = 0;
             int empWages = 0;
             int monthlyWages = 0;
+            int workingHr = 0;
+            int day = 0;
             //Creating object or instance of Random class
             Random random = new Random();
+          
 
-            //Calculatin monthly Wages for 20days
-            for (int day = 1; day <= TOTAL_WORKING_DAY; day++)
+            //Calculatin monthly Wages for 20days or for 100Hrs of work
+            while(day<=TOTAL_WORKING_DAY && workingHr<TOTAL_WORKING_HR)
             {
                 
 
@@ -53,9 +59,15 @@ namespace EmployeeWagesComputation
                 //calculating Daily wages of Employee by Working Hours
                 empWages = empHrs * EMP_RATE_PER_HR;
                 monthlyWages = monthlyWages + empWages;
+                workingHr += empHrs;
+                if(empInput!=0)
+                {
+                    day++;
+                }
+
             }
 
-            Console.WriteLine(" Wages of the Employee for "+TOTAL_WORKING_DAY+" days = "+monthlyWages);
+            Console.WriteLine(" Wages of the Employee for "+workingHr+ " Hour or "+day+" days = "+monthlyWages);
             Console.Read();
         }
     }
