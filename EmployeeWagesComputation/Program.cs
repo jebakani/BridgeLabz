903978bd9663b96f0c4738cbc616e30702c39387
Jebakani <vjishwarya@gmail.com>
2021-06-28 10:58:40 +0530

    [JEBAKANI] Creating Employee wages computation

---
+++ EmployeeWagesComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWagesComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages Computation");
+            Console.Read();
+        }
+    }
+}
