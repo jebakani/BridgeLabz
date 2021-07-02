a18775a6c5cbdf0277293df86b8622cbf68fdc9b
Jebakani <vjishwarya@gmail.com>
2021-07-02 12:43:47 +0530

    [JEBAKANI] UC8-Calculating wages for multiple companies

--- EmployeeWagesComputation/Program.cs
+++ EmployeeWagesComputation/Program.cs
@@ -7,9 +7,6 @@ namespace EmployeeWagesComputation
         //adding constant global variable
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
-        const int EMP_RATE_PER_HR = 20;
-        const int TOTAL_WORKING_DAY = 20;
-        const int TOTAL_WORKING_HR = 100;
 
 
         /// <summary>
@@ -20,13 +17,14 @@ namespace EmployeeWagesComputation
         /// UC5- Calculating Monthly Wages of Employee
         /// UC6- Calculating The wages till the condition of 100hr or for 20days
         /// UC7- Computing the Employee wages using class method
+        /// UC8- Computing the employee wages for multiple company
         /// </summary>
         /// <param name="args">The arguments.</param>
         /// 
 
 
         //class method to compute the employee wages
-        public static void ComputeEmployeeWage()
+        public static void ComputeEmployeeWage(String companyName,int employeeRatePerHr,int maxWorkingDays, int maxWorkingHrs)
         {
             //initialize local variable 
             int empHrs = 0;
@@ -39,7 +37,7 @@ namespace EmployeeWagesComputation
 
 
             //Calculatin monthly Wages for 20days or for 100Hrs of work
-            while (day <= TOTAL_WORKING_DAY && workingHr < TOTAL_WORKING_HR)
+            while (day <= maxWorkingDays && workingHr < maxWorkingHrs)
             {
 
 
@@ -61,7 +59,7 @@ namespace EmployeeWagesComputation
                 }
 
                 //calculating Daily wages of Employee by Working Hours
-                empWages = empHrs * EMP_RATE_PER_HR;
+                empWages = empHrs * employeeRatePerHr;
                 monthlyWages = monthlyWages + empWages;
                 workingHr += empHrs;
 
@@ -72,15 +70,18 @@ namespace EmployeeWagesComputation
 
             }
 
-            Console.WriteLine(" Wages of the Employee for " + workingHr + " Hour or " + day + " days = " + monthlyWages);
+            Console.WriteLine("Total working days :{0} \nTotal working hours:{1}",day,workingHr);
+            Console.WriteLine("Total Employee wage for company \"{0}\" is :{1}\n\n", companyName, monthlyWages);
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation");
 
-            //calling the class method
-            ComputeEmployeeWage();
+            //calling the class method for multiple company
+            ComputeEmployeeWage("Reliance",20,25,100);
+            ComputeEmployeeWage("Big-Bazaar",50, 30, 150);
+
             Console.Read();
         }
     }
