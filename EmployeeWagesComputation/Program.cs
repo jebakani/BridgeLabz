59203372e41a989fd30b5fe5e3cce86b49c7d76d
Jebakani <vjishwarya@gmail.com>
2021-07-04 11:02:40 +0530

    [JEBAKANI] UC9-Storing total wages for each company

--- EmployeeWagesComputation/Program.cs
+++ EmployeeWagesComputation/Program.cs
@@ -4,9 +4,7 @@ namespace EmployeeWagesComputation
 {
     class Program
     {
-        //adding constant global variable
-        const int IS_FULL_TIME = 1;
-        const int IS_PART_TIME = 2;
+       
 
 
         /// <summary>
@@ -18,70 +16,27 @@ namespace EmployeeWagesComputation
         /// UC6- Calculating The wages till the condition of 100hr or for 20days
         /// UC7- Computing the Employee wages using class method
         /// UC8- Computing the employee wages for multiple company
+        /// UC9- Saving total wages for each company using class EmployeeBuilder
         /// </summary>
         /// <param name="args">The arguments.</param>
         /// 
 
 
         //class method to compute the employee wages
-        public static void ComputeEmployeeWage(String companyName,int employeeRatePerHr,int maxWorkingDays, int maxWorkingHrs)
-        {
-            //initialize local variable 
-            int empHrs = 0;
-            int empWages = 0;
-            int monthlyWages = 0;
-            int workingHr = 0;
-            int day = 0;
-            //Creating object or instance of Random class
-            Random random = new Random();
-
-
-            //Calculatin monthly Wages for 20days or for 100Hrs of work
-            while (day <= maxWorkingDays && workingHr < maxWorkingHrs)
-            {
-
-
-                //Generating Random value by calling Next Method
-                int empInput = random.Next(0, 3);
-
-                //Checking employee status using switch case statement
-                switch (empInput)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-
-                //calculating Daily wages of Employee by Working Hours
-                empWages = empHrs * employeeRatePerHr;
-                monthlyWages = monthlyWages + empWages;
-                workingHr += empHrs;
-
-                if (empInput != 0)
-                {
-                    day++;
-                }
-
-            }
-
-            Console.WriteLine("Total working days :{0} \nTotal working hours:{1}",day,workingHr);
-            Console.WriteLine("Total Employee wage for company \"{0}\" is :{1}\n\n", companyName, monthlyWages);
-        }
+        
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation");
-
-            //calling the class method for multiple company
-            ComputeEmployeeWage("Reliance",20,25,100);
-            ComputeEmployeeWage("Big-Bazaar",50, 30, 150);
-
+            //creating object for each company
+            EmployeeWageBuilder reliance = new EmployeeWageBuilder("Reliance", 20, 25, 100);
+            EmployeeWageBuilder bigBazaar = new EmployeeWageBuilder("Big-Bazaar", 50, 30, 150);
+            //computing employee wages for reliance company
+            reliance.ComputeEmployeeWage();
+            Console.WriteLine(reliance.toString());
+            //computing employee wages for bigbazaar company
+            bigBazaar.ComputeEmployeeWage();
+            Console.WriteLine(bigBazaar.toString());
             Console.Read();
         }
     }
